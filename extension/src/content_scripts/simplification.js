f87f4770b1bfd4ef1730261d2f6393d75054e171
Justin Obara <obarajustin@gmail.com>
2017-06-12 12:38:27 -0400

    GPII-2432: Injecting a toggle button to show/hide navigation

--- extension/src/content_scripts/simplification.js
+++ extension/src/content_scripts/simplification.js
@@ -19,22 +19,38 @@
     fluid.defaults("gpii.simplify", {
         gradeNames: ["fluid.viewComponent"],
         selectors: {
+            navToggle: ".gpii-simplify-navToggle",
             article: "article, [role~='article'], .article, #article",
             main: "main, [role~='main'], .main, #main",
             genericContent: ".content, #content, .body:not('body'), #body:not('body')",
             nav: "nav, [role~='navigation'], .navigation, .nav, #nav, #navigation, [role~='search'], [type~='search']"
         },
+        markup: {
+            navToggle: "<button class='gpii-simplify-navToggle'></button>"
+        },
+        strings: {
+            navToggle: "Toggle Navigation"
+        },
         model: {
-            simplify: false
+            simplify: false,
+            showNav: false
         },
         modelListeners: {
             simplify: {
                 listener: "{that}.set",
                 args: ["{change}.value"]
+            },
+            showNav: {
+                listener: "{that}.toggleNav",
+                args: ["{change}.value"],
+                excludeSource: "init"
             }
         },
-        events: {
-            settingChanged: null
+        listeners: {
+            "onCreate.toggleNav": {
+                listener: "{that}.toggleNav",
+                args: ["{that}.model.showNav"]
+            }
         },
         invokers: {
             findContent: {
@@ -47,6 +63,10 @@
             },
             set: {
                 funcName: "gpii.simplify.set",
+                args: ["{that}", "{arguments}.0"]
+            },
+            toggleNav: {
+                funcName: "gpii.simplify.toggleNav",
                 args: ["{that}", "{arguments}.0"]
             }
         }
@@ -66,14 +86,42 @@
         return elms;
     };
 
+    gpii.simplify.injectToggle = function (that, content) {
+        var navToggle = that.locate("navToggle");
+
+        if (!navToggle.length) {
+            navToggle = $(that.options.markup.navToggle);
+            navToggle.attr("aria-pressed", that.model.showNav);
+            navToggle.text(that.options.strings.navToggle);
+            navToggle.click(function () {
+                var newState = !that.model.showNav;
+                that.applier.change("showNav", newState);
+                navToggle.attr("aria-pressed", newState);
+            });
+            content.prepend(navToggle);
+        }
+    };
+
     gpii.simplify.set = function (that, state) {
         var content = that.findContent();
         if (state && content.length) {
             that.container.css("visibility", "hidden");
             content.css("visibility", "visible");
+            gpii.simplify.injectToggle(that, content);
+            that.locate("navToggle").show();
         } else {
+            that.locate("navToggle").hide();
             that.container.css("visibility", "");
             content.css("visibility", "");
+        }
+    };
+
+    gpii.simplify.toggleNav = function (that, state) {
+        var nav = that.findNav();
+        if (state && that.model.simplify) {
+            nav.css("visibility", "visible");
+        } else {
+            nav.css("visibility", "");
         }
     };
 })(jQuery, fluid);
