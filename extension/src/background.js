2c3cdc7cd317ee9955078aa9698d4199d8f4a634
Justin Obara <obarajustin@gmail.com>
2017-11-08 15:21:43 -0500

    GPII-2424: Removing log statement

--- extension/src/background.js
+++ extension/src/background.js
@@ -16,6 +16,4 @@
 
 fluid.setLogging(true);
 var gpii = fluid.registerNamespace("gpii");
-var chromeSettings = gpii.chrome.settings();
-
-fluid.log("Instantiated chromeSettings:", chromeSettings);
+gpii.chrome.settings();
