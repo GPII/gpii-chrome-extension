36367854c96acdf24f6d0b722309dad4e4de433c
Justin Obara <obara.justin@gmail.com>
2019-09-09 13:10:00 -0400

    GPII-4109: Moving to a centralized copyright

--- tests/browser/testem.js
+++ tests/browser/testem.js
@@ -1,3 +1,15 @@
+/*
+ * Copyright The UIO+ copyright holders
+ * See the AUTHORS.md file at the top-level directory of this distribution and at
+ * https://github.com/GPII/gpii-chrome-extension/blob/master/AUTHORS.md.
+ *
+ * Licensed under the New BSD license. You may not use this file except in
+ * compliance with this license.
+ *
+ * You may obtain a copy of the license at
+ * https://github.com/GPII/gpii-chrome-extension/blob/master/LICENSE.txt
+ */
+
 "use strict";
 var fluid = require("infusion");
 fluid.setLogging(true);
