728e3911894741f082003bfecdcf181823a9f784
Justin Obara <obara.justin@gmail.com>
2019-11-27 12:08:19 -0500

    GPII-4235: Attempt to see if the HEADLESS env var works for Ubuntu runs

--- tests/browser/testem.js
+++ tests/browser/testem.js
@@ -36,7 +36,7 @@ fluid.defaults("fluid.tests.testem", {
         tests:   "%ui-options-chrome/tests"
     },
     testemOptions: {
-        launch: "Firefox",
+        launch: "Chrome",
         disable_watching: true,
         tap_quiet_logs: true
     }
